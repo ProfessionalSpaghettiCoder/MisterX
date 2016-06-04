a15a5ee98c83946dd9b4d65499f934807a48539e
ProfessionalSpaghettiCoder <pixelbrei123456@gmail.com>
2016-06-04 23:57:02 +0200

    HelloWorld
    Added a hello world test class

---
+++ MisterX/src/misterX/test/HelloWorld_Test.java
@@ -0,0 +1,13 @@
+package misterX.test;
+
+public class HelloWorld_Test {
+	
+	public HelloWorld_Test() {
+		System.out.println("Hello World");
+	}
+	
+	public static void main(String[] args) {
+		HelloWorld_Test();
+	}
+	
+}
